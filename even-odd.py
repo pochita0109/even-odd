a4b2d9b99a2989b9588648e7dde0e71561202cd0
pochita0109 <akosicosta@gmail.com>
2023-04-25 11:24:58 -0700

    First commit, Pseudocode

---
+++ even-odd.py
@@ -0,0 +1,16 @@
+# Kenneth John Costa
+# Assignment 4
+# Even-Odd
+
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt 
+# that will contains all even numbers extracted from the numbers.txt. 
+# The second text file will be named odd.txt that will contains all odd numbers 
+# extracted from the numbers.txt.
+
+# Open numbers.txt, even.txt, and odd.txt
+# Read numbers.txt
+# If the integers are even:
+# Write to even.txt
+# If the integers are odd:
+# Write to odd.txt
